0e5dfe3311219c6028407074d4cff3f2d97c8ae7
Yulia2703 <giuli.k@libero.it>
2025-02-01 14:44:17 +0100

    first01

--- assets/index.js
+++ assets/index.js
@@ -1,34 +1,35 @@
 const text = document.getElementById("text");
-//console.log("text");
-const onlyText = text.textContent;
-//console.log("onlyText");
+console.log(text);
+
+const onlyText = text.innerText;
+console.log(onlyText);
+
 const textLenght = onlyText.length;
-//console.log("textLenght");
+console.log(textLenght);
 
 const result = onlyText.replace(/;/g,'\n');
-//console.log("result");
+console.log(result);
 
 const substring = result.replace(/\s+/g,'');
-//console.log("substring");
+console.log(substring);
 
 const newText = result.slice(28,50);
-//console.log("newText");
+console.log(newText);
 
 const replacedText = newText.replace("клён","дубе");
-//console.log("replacedText");
+console.log(replacedText);
 
 const replacedText2 = result.toUpperCase();
-//console.log("replacedText2");
+console.log(replacedText2);
 
 const replacedText3 = result.replace(/клён/g,"дуб");
-//console.log("replacedText3");
+console.log(replacedText3);
 
 const index = onlyText.indexOf("моря");
-//console.log("index");
+console.log(index);
 
-const modifiedText =() => {
 const findLetter = replacedText[0];
 const upperLetter = findLetter.toUpperCase();
 const slisedText = replacedText.slice(1);
-
-} 
+const modifiedText = (upperLetter + slisedText);
+console.log(modifiedText);
